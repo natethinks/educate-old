3966a49ab1c1f33be587c0e09d28bfd60ce55bd4
Nate Meyers <nate@nates-mbp.home>
2017-08-23 09:06:09 +0200

    added new auth component, working on routes strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: true });
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule
