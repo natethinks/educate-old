3cef46c529cccc019279dc142623c860c5e88978
Nate Meyers <nate@Nates-MacBook-Pro.local>
2017-09-08 15:28:48 -0600

    moved everything into a client folder

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: true });
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
