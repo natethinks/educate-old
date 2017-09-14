15156c564e3bb0480c25f215b2a09ee1d65fd316
Nate Meyers <nate@Nates-MacBook-Pro.local>
2017-09-13 21:25:47 -0600

    Added routes for api endpoints

--- api/router.go
+++ api/router.go
@@ -39,3 +39,105 @@ func Something(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the something page")
 	return
 }
+
+// User functions
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("GetUsers")
+	return
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("GetUser")
+	return
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("CreateUser")
+	return
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("DeleteUser")
+	return
+}
+
+// Subject functions
+
+func GetSubjects(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("GetSubjects")
+	return
+}
+
+func GetSubject(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("")
+	return
+}
+
+func GetSubjectsQuery(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("")
+	return
+}
+
+func CreateSubject(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("")
+	return
+}
+
+func DeleteSubject(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("")
+	return
+}
+
+// Topic functions
+
+func GetTopics(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("")
+	return
+}
+
+func GetTopic(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("")
+	return
+}
+
+func GetTopicsQuery(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("")
+	return
+}
+
+func CreateTopic(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("")
+	return
+}
+
+func DeleteTopic(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("")
+	return
+}
+
+//ResourceFunctions
+
+func GetResources(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("")
+	return
+}
+
+func GetResource(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("")
+	return
+}
+
+func GetResourceQuery(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("")
+	return
+}
+
+func CreateResource(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("")
+	return
+}
+
+func DeleteResource(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("")
+	return
+}
--- api/routes.go
+++ api/routes.go
@@ -23,7 +23,34 @@ func LoadRoutes() *mux.Router {
 	router.HandleFunc("/api/test", Test).Methods("GET")
 	router.HandleFunc("/api/test", Something).Methods("GET")
 
-	SetRoute("POST", "/secure", Secure)
+	// User functions
+	router.HandleFunc("/api/user", GetUsers).Methods("GET")
+	router.HandleFunc("/api/user/{id}", GetUser).Methods("GET")
+	router.HandleFunc("/api/user", CreateUser).Methods("POST")
+	router.HandleFunc("/api/user", DeleteUser).Methods("DELETE")
+	// Administration functions
+	// is this even necessary? Don't know
+	// could be good for promoting people to admin?
+	// Subject functions
+	router.HandleFunc("/api/subject", GetSubjects).Methods("GET")
+	router.HandleFunc("/api/subject/{slug}", GetSubject).Methods("GET")
+	router.HandleFunc("/api/subject/{query}", GetSubjectsQuery).Methods("GET")
+	router.HandleFunc("/api/subject", CreateSubject).Methods("POST")
+	router.HandleFunc("/api/subject", DeleteSubject).Methods("DELETE")
+	// Topic functions
+	router.HandleFunc("/api/topic", GetTopics).Methods("GET")
+	router.HandleFunc("/api/topic/{slug}", GetTopic).Methods("GET")
+	router.HandleFunc("/api/topic/{query}", GetTopicsQuery).Methods("GET")
+	router.HandleFunc("/api/topic", CreateTopic).Methods("POST")
+	router.HandleFunc("/api/topic", DeleteTopic).Methods("POST")
+	// Resource functions
+	router.HandleFunc("/api/resource", GetResources).Methods("GET")
+	router.HandleFunc("/api/resource/{slug}", GetResource).Methods("GET")
+	router.HandleFunc("/api/resource/{query}", GetResourceQuery).Methods("GET")
+	router.HandleFunc("/api/resource", CreateResource).Methods("POST")
+	router.HandleFunc("/api/resource", DeleteResource).Methods("DELETE")
+
+	SetRoute("POST", "/api/secure", Secure)
 
 	return router
 }
@@ -33,12 +60,21 @@ func LoadRoutes() *mux.Router {
 // 	router.HandleFunc(path, fu).Methods(method)
 // }
 
+// func SecureRoute(method string, path string, fu Handler) {
+// 	fmt.Println("SecureRoute")
+// 	something := r.Header.Get("something")
+// 	fmt.Println(something)
+// 	router.Handle(path, fu).Methods(method)
+// }
+
 // SetRoute something
 func SetRoute(method string, path string, fu Handler) {
+	fmt.Println("SetRoute")
 	router.Handle(path, authedHandler(fu)).Methods(method)
 }
 
 func authedHandler(fu Handler) *negroni.Negroni {
+	fmt.Println("authedHandler")
 	return negroni.New(
 		negroni.HandlerFunc(handlerWithNext),
 		negroni.Wrap(http.HandlerFunc(fu)),
@@ -48,14 +84,18 @@ func authedHandler(fu Handler) *negroni.Negroni {
 // handlerwithnext is where my control really lies. This is where I check the token
 
 func handlerWithNext(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	fmt.Println("handlerWithNext")
 	err := checkSession(w, r)
+	fmt.Println(err)
 
 	// If there was an error, do not call next.
 	if err == nil && next != nil {
+		fmt.Println("something went wrong")
 		next(w, r)
 	}
 }
 
 func checkSession(w http.ResponseWriter, r *http.Request) error {
+	fmt.Println("checkSession")
 	return fmt.Errorf("Error extracting token: 1234")
 }
